227f7f4691daec30548f57d7f8344c18d94c94c2
mirantel <magi.mirantel@gmail.com>
2013-11-20 21:29:42 +0200

    created slides

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,36 +3,29 @@ module.exports = function(grunt) {
 
 grunt.initConfig({
 	stylus: {
-		compile: {
-			options: {
-				paths: ['path/to/import', 'another/to/import'],
-				urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
-				use: [
-					require('fluidity') // use stylus plugin at compile time
-					],
-				import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
-					'foo',       //  that is compiled. These might be findable based on values you gave
-					'bar/moo'    //  to `paths`, or a plugin you added under `use`
-					]
-			},
-			files: {
-				'path/to/result.css': 'path/to/source.styl', // 1:1 compile
-				'path/to/another.css': ['path/to/sources/*.styl', 'path/to/more/*.styl'] // compile and concat into single file
-			}
-		}
-	},
-	copy: {
-		main: {
-			files: [
-				{expand: true, cwd: 'src/img/', src: ['**'], dest: 'production/img/'},
-				{expand: true, cwd: 'src/css/', src: ['screen.css'], dest: 'production/css/'},
-				{expand: true, cwd: 'src/js/', src: ['**'], dest: 'production/js/'}
-			]
+		options: {
+			compress: false,
 		},
-		css: {
+		global: {
+			files: {
+				'production/css/screen.css': 'src/styl/screen.styl'
+			},
+		},
+	},
+
+	autoprefixer: {
+		options: {
+			 browsers: ['last 3 version', 'ie >= 8']
+		},
+		global: {
+			src: 'production/css/styles.css'
+		},
+	},
+
+	copy: {
+		img: {
 			files: [
-				{expand: true, cwd: 'src/img/', src: ['**'], dest: 'production/img/'},
-				{expand: true, cwd: 'src/css/', src: ['**'], dest: 'production/css/'},
+				{expand: true, cwd: 'src/img/', src: ['**'], dest: 'production/img/'}
 			]
 		},
 		js: {
@@ -51,16 +44,23 @@ grunt.initConfig({
 			dest: 'production/'
 		}
 	},
+
+	clean: {
+		html: ["production/_*.html"],
+		release: ["production"],
+	},
+
 	csso: {
 		dist: {
-			src: 'src/css/screen.css',
+			src: 'production/css/screen.css',
 			dest:'production/css/screen.min.css'
 		}
 	},
+
 	watch: {
 		css: {
 			files: 'src/styl/*.styl',
-			tasks: ['stylus','copy:css','clean:css'],
+			tasks: ['stylus', 'autoprefixer'],
 		},
 		js: {
 			files: 'src/js/*.js',
@@ -71,15 +71,10 @@ grunt.initConfig({
 			tasks: ['includereplace', 'clean:html'],
 		},
 	},
-	clean: {
-		html: ["production/_*.html"],
-		css: ["src/css"],
-		dev: ["production/_*.html", "src/css"],
-		release: ["production"],
-		after: ["production/_*.html", "src/css"]
-	}
+
 });
 	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -87,6 +82,6 @@ grunt.initConfig({
 	grunt.loadNpmTasks('grunt-csso');
 
 	grunt.registerTask( 'default', ['watch']);
-	grunt.registerTask( 'release', ['clean:release', 'compass', 'copy:main', 'includereplace', 'csso', 'clean:after']);
+	grunt.registerTask( 'release', ['clean:release', 'stylus', 'copy', 'includereplace', 'csso', 'clean:html']);
 
 };
